2419420e3f6e413c979929f1ad76adb68f8b7d4d
ysoktsk <hnkc.ysoktsk@gmail.com>
2024-01-07 22:19:03 +0900

    initial

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,12 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
     "plugin:testing-library/react",
     "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs", "setupTests.js", "__mocks__"],
   parser: "@typescript-eslint/parser",
@@ -15,6 +18,12 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
   },
   overrides: [
     {
